0d804d515be17b84fa515992764600ac776bad43
NathanRusso <nathandanielrusso@gmail.com>
2026-08-23 00:05:22 -0400

    I am reducing my workers from 4 to 2.

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,4 +1,4 @@
 bind = "0.0.0.0:8000"   # Listen for request on every network interface available inside this container
-workers = 4             # The number of worker process to spawn
+workers = 2             # The number of worker process to spawn
 accesslog = "-"         # Logs standard output
 errorlog = "-"          # Logs standard error
