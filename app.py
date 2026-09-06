69a46975bba6a4480ce9b6d5b58eae611fdeda1a
NathanRusso <nathandanielrusso@gmail.com>
2026-09-05 23:19:49 -0400

    Removed unneeded lines.

--- app.py
+++ app.py
@@ -10,15 +10,17 @@ import os
 
 
 COURSE_QUERY = (
+    "SELECT c.quarter, d.code, c.course, c.credits, c.title, c.description, c.prerequisites, c.typically_offered, c.contact_hours "
+    "FROM courses c JOIN departments d ON c.department = d.id "
+    "WHERE d.code = ? AND c.course = ? ORDER BY c.quarter DESC LIMIT 1"
+)
+"""
+COURSE_QUERY = (
     "SELECT c.quarter, d.code, c.course, c.credits, c.title, c.description "
     "FROM courses c JOIN departments d ON c.department = d.id "
     "WHERE d.code = ? AND c.course = ? ORDER BY c.quarter DESC LIMIT 1"
 )
-#COURSE_QUERY = (
-#    "SELECT c.quarter, d.code, c.course, c.credits, c.title, c.description, c.prerequisites, c.typically_offered, c.contact_hours "
-#    "FROM courses c JOIN departments d ON c.department = d.id "
-#    "WHERE d.code = ? AND c.course = ? ORDER BY c.quarter DESC LIMIT 1"
-#)
+"""
 pool = None
 
 # Startup code
